d2559a1fea0862e2bd9574e26f330ab6e1209743
Buck Pendleton <buckpendleton@gmail.com>
2021-02-12 13:09:31 -0500

    uploaded file

---
+++ app2.js
@@ -0,0 +1,24 @@
+var num1 = prompt(`Enter a Number`);
+console.log(num1);
+
+var num2 = prompt('Enter another number');
+console.log(num2);
+
+result = alert (Math.pow(num1 , num2));
+
+console.log(Math.floor(Math.random() * 3) + 1);
+
+var alphabet = [`a`, `b`, `c`,`d`,`e`,`f`];
+console.log(alphabet);
+
+alphabet.pop(`f`);
+console.log(alphabet);
+
+alphabet.push(`g`);
+console.log(alphabet);
+
+alphabet.shift(`a`);
+console.log(alphabet);
+
+alphabet.unshift(`1`);
+console.log(alphabet);
