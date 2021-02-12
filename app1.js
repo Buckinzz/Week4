77e551d74657892455d41dc342847d627b1e3772
Buck Pendleton <buckpendleton@gmail.com>
2021-02-11 22:27:12 -0500

    uploaded files

---
+++ app1.js
@@ -0,0 +1,39 @@
+var x = ('Hello World')
+console.log('Hello World');
+
+var warn =("This is a warning!")
+console.warn('This is a warning!');
+
+var error =('Error! System Critical!')
+console.error('Error! System Critical!')
+
+var var1 =('Value')
+console.log(var1)
+
+console.log();
+
+var var2 = 5;
+console.log(var1 + var2) // Value5
+
+/*Numbers
+Booleans
+Type Coercion
+Array Methods
+indexOf */
+
+console.log('String'); //String
+
+var var3 =(`Text`);
+console.log(var3);
+
+var3 = true;
+console.log(var3);
+
+var1 = 5;
+console.log(var1);
+
+var2 = (`3`);
+console.log(var2);
+
+var var2 =parseInt(var1);
+console.log(var2);
